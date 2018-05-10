5702b57c6433acc0c6aa53c9efcddbcd7d977cd3
Andreas Sacher <github@a-sacher.de>
2018-05-10 20:50:12 +0200

    Add build.py, update data-final-fixes.lua to allow DRY code, change icons for electronic-circuit 1 to 3 by readding them

---
+++ build.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import zipfile
+import json
+
+def zip(src, dst):
+    zf = zipfile.ZipFile("{}.zip".format(dst), "w", zipfile.ZIP_DEFLATED)
+    abs_src = os.path.abspath(src)
+    for dirname, subdirs, files in os.walk(src):
+        for filename in files:
+            absname = os.path.abspath(os.path.join(dirname, filename))
+            arcname = os.path.join(dst, absname[len(abs_src) + 1:])
+            print('zipping {} as {}'.format(os.path.join(dirname, filename), arcname))
+            zf.write(absname, arcname)
+    zf.close()
+
+with open('src/info.json') as json_data:
+    modInfo = json.load(json_data)
+    zip("src", "{}_{}".format(modInfo['name'], modInfo['version']))
+    json_data.close()
