ec339d8b3ac62acfa0112ba9df889e69dc1fc116
Andreas Sacher <github@a-sacher.de>
2018-06-10 15:04:01 +0200

    Add Boron Trioxide, Bone Meal, Lime, Borax, Ralesia Seeds, Treated Wood, Optical Set, Ground Sample 01, Printed Circuit Substrate 1, Fiberboard, Fawogae Substrate and Graphite

--- src/migrations/shared.lua
+++ src/migrations/shared.lua
@@ -5,7 +5,16 @@ shared.STACKABLES = {
     { item = "coke", icon = nil, stage = 1 },
     { item = "fawogae", icon = nil, stage = 1 },
     { item = "iron-oxide", icon = nil, stage = 1 },
-    { item = "ralesia", icon = nil, stage = 1 }
+    { item = "ralesia", icon = nil, stage = 1 },
+    { item = "ralesia-seeds", icon = nil, stage = 1 },
+    { item = "fawogae-substrate", icon = nil, stage = 1 },
+    { item = "boron-trioxide", icon = nil, stage = 1 },
+    { item = "bonemeal", icon = nil, stage = 1 },
+    { item = "lime", icon = nil, stage = 1 },
+    { item = "borax", icon = nil, stage = 1 },
+    { item = "treated-wood", icon = nil, stage = 1 },
+    { item = "ground-sample01", icon = nil, stage = 1 },
+    { item = "lens", icon = nil, stage = 1 }
   },
   pyhightech = {
     { item = "clay", icon = nil, stage = 1 },
@@ -16,10 +25,14 @@ shared.STACKABLES = {
     { item = "melamine-resin", icon = nil, stage = 1 },
     { item = "electronic-circuit", icon = nil, stage = 2 },
     { item = "advanced-circuit", icon = nil, stage = 2 },
-    { item = "processing-unit", icon = nil, stage = 3 }
+    { item = "processing-unit", icon = nil, stage = 3 },
+    { item = "graphite", icon = nil, stage = 1 },
+    { item = "fiberboard", icon = nil, stage = 1 },
+    { item = "pcb1", icon = nil, stage = 1 }
   }
 }
 shared.STACKING_PREFIX = "deadlock-stacking-"
 shared.CRATING_PREFIX ="deadlock-crating-"
 
 return shared
+
